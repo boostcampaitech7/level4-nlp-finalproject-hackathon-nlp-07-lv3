91608ff6d7ccd915d193789b12afeab3dc2a1923
doraemon500 <pkt45349048@gmail.com>
2025-01-27 16:53:03 +0900

    chore: class name change

--- src/Multi-Level-OT-main/policies/__init__.py
+++
@@ -1,4 +0,0 @@
-from policies.mixed_precision import *
-from policies.wrapping import *
-from policies.activation_checkpointing_functions import apply_fsdp_checkpointing
-from policies.anyprecision_optimizer import AnyPrecisionAdamW
