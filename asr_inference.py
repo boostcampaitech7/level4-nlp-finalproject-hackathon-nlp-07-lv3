51b112e560ff5388ddb6c1342596ddcf272ca435
doraemon500 <pkt45349048@gmail.com>
2025-02-07 17:59:37 +0900

    fix: ruff check W293 W292 issues

--- asr_inference.py
+++ asr_inference.py
@@ -4,4 +4,3 @@ import sys
 
 if __name__ == "__main__":
     subprocess.run([f'{sys.executable}', "src/evaluate.py", "--cfg-path", "src/configs/eval_config.yaml", "--mode", "submission_asr", "--skip_scoring"])
-    
